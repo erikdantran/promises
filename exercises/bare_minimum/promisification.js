82535c741475c69c91944bdc1151c5d792735793
Brett Roberts <brettr2010@gmail.com>
2021-10-12 12:36:39 -0500

    Completed promiseConstructor.

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -1,7 +1,7 @@
 /**
  * Create the promise returning `Async` suffixed versions of the functions below,
  * Promisify them if you can, otherwise roll your own promise returning function
- */ 
+ */
 
 var fs = require('fs');
 var request = require('needle');
@@ -29,7 +29,21 @@ var getGitHubProfile = function (user, callback) {
   });
 };
 
-var getGitHubProfileAsync; // TODO
+
+// var pluckFirstLineFromFileAsync = function (filePath) {
+//   var readFile = Promise.promisify(fs.readFile);
+
+//   readFile(filePath, 'utf8')
+//     .then((text) => {
+//       var firstLine = text.toString().split('\n')[0];
+//       return firstLine;
+//     })
+//     .catch(err => {
+//       // callback(err);
+//     });
+// };
+
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile);
 
 
 // (2) Asyncronous token generation
@@ -47,7 +61,7 @@ var generateRandomTokenAsync; // TODO
 var readFileAndMakeItFunny = function(filePath, callback) {
   fs.readFile(filePath, 'utf8', function(err, file) {
     if (err) { return callback(err); }
-   
+
     var funnyFile = file.split('\n')
       .map(function(line) {
         return line + ' lol';
